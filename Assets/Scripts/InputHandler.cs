27a7724ffdee6d9f6d523be2068f5302bdb9e9ce
Somn <somnacin7@gmail.com>
2016-12-11 19:28:48 -0500

    add char and jumping

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour {
+
+    public delegate void JumpDown();
+    public event JumpDown OnJumpDown; 
+    private bool jump;
+
+    public float horizontalAxis;
+
+	// Use this for initialization
+	void Start() {
+	    
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxis("Jump") > 0) {
+            if (!jump) {
+                if (OnJumpDown != null) {
+                    OnJumpDown();
+                }
+                jump = true;
+            }
+        } else {
+            jump = false;
+        }
+	}
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+    public float jumpForce = 0;
+
+    private InputHandler input;
+    private bool jump;
+    private Rigidbody2D rb2d;
+
+    void Awake() {
+        input = GetComponent<InputHandler>();
+        input.OnJumpDown += SetJump;
+
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate() {
+        if (jump) {
+            var curVelocity = rb2d.velocity;
+            rb2d.velocity = new Vector2(curVelocity.x, jumpForce);
+            jump = false;
+        }
+    }
+
+    void SetJump() {
+        jump = true;
+    }
+}
